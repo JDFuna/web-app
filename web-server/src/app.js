2b8f358ba64fea16603f4f08f15878d1c6d156d8
Ma. Victoria G. Pamplona <mvgersaliapamplona8719@gmail.com>
2021-10-31 20:22:27 +0700

    query string + json http endpoint

--- web-server/src/app.js
+++ web-server/src/app.js
@@ -1,22 +1,114 @@
+
 const path = require('path')
 const express = require('express')
+const hbs = require('hbs')
+const geocode = require('./utils/geocode')
+const forecast = require('./utils/forecast')
+
 
 
 console.log(__dirname)
 
-console.log(path.join(__dirname, '../public'))
-
 const app = express()
-const publicDirectoryPath = path.join(__dirname, '../public')
 
+// Define paths for Express Config
+const publicDirectoryPath = path.join(__dirname, '../public')
+const viewsPath = path.join(__dirname,'../templates/views')
+const partialsPath= path.join(__dirname, '../templates/partials')
+
+
+// Set Handlebars engine and views location
+app.set('view engine', 'hbs')
+app.set('views', viewsPath)
+hbs.registerPartials(partialsPath)
+
+// Setup Static directory to server
 app.use(express.static(publicDirectoryPath))
 
 
+
+// app.com
+app.get('', (req, res) =>{
+    res.render('index',{
+        title: 'Weather',
+        name: "Judy D. Funa"
+    })
+})
+
+// app.com/help
+app.get('/help', (req, res) =>{
+    res.render('help',{
+        helpText: ' This is the text',
+        title: 'Help',
+        name: 'Judy D. Funa'
+    })
+})
+
+// app.com/about
+app.get('/about', (req, res) =>{
+    res.render('about',{
+        title: "About Me",
+        name: "Judy D. Funa"
+
+    })
+})
+
 // app.com/weather
 app.get('/weather', (req, res) =>{
+    if(!req.query.address){
+        return res.send({
+            error: 'Must provide an address'
+        })
+    }
+    
+    geocode(req.query.address, (error, {latitude, longitude, location}) =>{
+        if (error){
+            return res.send({error})
+        }
+        forecast(latitude, longitude, (error, forecastData) => {
+            if(error){
+                return res.send({error})
+            }
+
+            res.send({
+                forecast: forecastData,
+                location,
+                address: req.query.address
+            })
+        })
+    })
+})
+
+app.get('/products', (req, res) =>{
+    if (!req.query.search){
+        return res.send({
+            error: 'Must provide search term'
+        })    
+    }
+    
+
+    console.log(req.query.search)
     res.send({
-        forecast: 'It is raining',
-        location: 'Bulan, Sorsogon'
+        products: []
+    })
+})
+
+// /help/404
+app.get('/help/*', (req, res) =>{
+    res.render('404',{
+        title:'404',
+        name: 'Judy D. Funa',
+        errorMessage: 'Help Article not found'
+    })
+})
+
+
+// 404
+app.get('*', (req, res) =>{
+    res.render("404",{
+        title: '404',
+        name: 'Judy D. Funa',
+        errorMessage: 'Page not found'
     })
 })
 
