bab019b3ab3d3551f63eba1ba88d40f0b2c82e5c
Judy Dolot Funa <89194168+JDFuna@users.noreply.github.com>
2021-10-31 18:40:54 +0700

    callback abstraction

--- weather-app/app.js
+++ weather-app/app.js
@@ -1,18 +1,17 @@
-const request = require('request')
 
-const geocodeURL = 'https://api.mapbox.com/geocoding/v5/mapbox.places/Bulan%20Sorsogon.json?access_token=pk.eyJ1IjoianVhbm1hdHRlbyIsImEiOiJja3VwdTJ3OHI0bzA4MndvOGtvZWhrY20wIn0.c2s4TtrNnvq0YfiXGoXp-g&limit=1'
+const geocode = require('./utils/geocode')
+const forecast = require('./utils/forecast')
 
-request({ url : geocodeURL, json:true}, (error, response) => {
-    if (error){
-        console.log('Unable to connect to the location services!')
-    }
-    else if(response.body.features.length === 0 ){
-        console.log('Location cannot be found. Try another location')
-    }
-    else{
-        const latitude = response.body.features[0].center[1]
-        const longitude = response.body.features[0].center[0]
-        console.log(latitude, longitude)
-}
-    
+
+
+geocode ('Prieto Diaz', (error, data) =>{
+    console.log('Error', error)
+    console.log('Data', data)
 })
+
+
+
+forecast(12.6650, 123.8877, (error, data) => {
+    console.log('Error', error)
+    console.log('Data', data)
+  })
