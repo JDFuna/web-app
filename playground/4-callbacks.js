443f015a4995e0e4caa3988a48515d60ad4abaee
Judy Dolot Funa <89194168+JDFuna@users.noreply.github.com>
2021-10-31 18:00:53 +0700

    Callback playground

--- playground/4-callbacks.js
+++ playground/4-callbacks.js
@@ -1,35 +1,48 @@
-// setTimeout(() => {
-//     console.log('The seconds are up')
+// setTimeout(() =>{
+//     console.log('Two seconds are up')
 // }, 2000)
 
-// const names = ['Andrew', 'Jen', 'Jess']
-// const  shortNames = names.filter((name) => {
+// const names = ['John', 'Mathew', 'Dino']
+// const shortName = names.filter ((name) =>{
 //     return name.length <= 4
 // })
 
-// const geocode = (address, callback) => {
-//     setTimeout(() => {
+// const geocode = (address, callback) =>{
+//     setTimeout (() =>{
 //         const data = {
 //                 latitude: 0,
 //                 longitude: 0
 //             }
-//             // return data 
+//             // return data
 //             callback(data)
-//     }, 2000)
+//     }, 2000)  
+    
+//     // const data = {
+//     //     latitude: 0,
+//     //     longitude: 0
+//     // }
+//     // return data
 // }
-
-// const data = geocode('Philadelphia', (data) => {
+// // const data = geocode('Philadephia')
+// // console.log(data)
+// geocode('Philadephia', (data) => {
 //     console.log(data)
 // })
-// console.log(data)
 
+//
+// Goal: Mess around with the callback pattern
+//
+// 1. Define an add function that accepts the correct arguments
+// 2. Use setTimeout to simulate a 2 second delay
+// 3. After 2 seconds are up, call the callback function with the sum
+// 4. Test your work!
 
-const add = (a, b, callback) => {
-    setTimeout(() => {
+const add = (a, b, callback) =>{
+    setTimeout(() =>{
         callback(a + b)
-    }, 2000);
+    }, 2000)
 }
+
 add(1, 4, (sum) => {
     console.log(sum) // Should print: 5
 })
-
