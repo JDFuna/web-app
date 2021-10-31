e6b563699aaf3a61d06db49fa690b7b65523e385
Kasandra G. Alvarez <kasandragalvarez12@sorsu.edu.ph>
2021-10-31 19:42:20 +0700

    Static Assets

--- web-server/src/app.js
+++ web-server/src/app.js
@@ -1,29 +1,16 @@
+const path = require('path')
 const express = require('express')
 
+
+console.log(__dirname)
+
+console.log(path.join(__dirname, '../public'))
+
 const app = express()
+const publicDirectoryPath = path.join(__dirname, '../public')
 
-// app.com
-app.get('', (req, res) =>{
-    res.send('<h1> Weather </h1>')
-})
+app.use(express.static(publicDirectoryPath))
 
-// app.com/help
-app.get('/help', (req, res) =>{
-    // res.send({
-    //     name: 'Mathew',
-    //     age: 21
-    // })
-    res.send([{
-        name: 'Mathew',
-    },{
-        name: 'Juan'
-    }])
-})
-
-// app.com/about
-app.get('/about', (req, res) =>{
-    res.send('<h1>About</h1>')
-})
 
 // app.com/weather
 app.get('/weather', (req, res) =>{
