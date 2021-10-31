7e62e1fbe49458ebe0f40e77ebf3a6cb44838807
Kasandra G. Alvarez <kasandragalvarez12@sorsu.edu.ph>
2021-10-31 19:16:16 +0700

    HTTP requests without library

--- playground/6-raw-http.js
+++ playground/6-raw-http.js
@@ -1,18 +1,24 @@
 const http = require('http')
-const url = 'http://api.weatherstack.com/current?access_key=029fb500b2b64b0652d7f09c39c7af5e/40,-75&units=f'
 
-const request = http.request(url, (response) => {
+const url = 'http://api.weatherstack.com/current?access_key=a3371e89db999a6f68a7a908b9195a56&query=40,-75'
+
+const request = http.request(url, (response) =>{
+
     let data = ''
-  response.on('data', (chunk) => {
-    data = data + chunk.toString()
-    // console.log(chunk)
-  })  
-  response.on('end', () => {
-    // console.log(data)
-    const body = JSON.parse(data)
-    console.log(body)
-  })
+
+    response.on('data', (chunk) =>{
+        data = data + chunk.toString()
+        console.log(chunk)
+    })
+
+    response.on('end', () =>{
+        // console.log(data)
+        const body = JSON.parse(data)
+        console.log(body)
+
+    })
 })
+
 request.on('error', (error) => {
     console.log('An error', error)
 })
