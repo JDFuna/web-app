1b763b23d51df48e1e92c4c259a007f7ee521992
Judy Dolot Funa <89194168+JDFuna@users.noreply.github.com>
2021-10-31 18:21:54 +0700

    Callback Chaining + dynamic location

--- weather-app/app.js
+++ weather-app/app.js
@@ -2,7 +2,6 @@ const request = require('request')
 
 const geocodeURL = 'https://api.mapbox.com/geocoding/v5/mapbox.places/Bulan%20Sorsogon.json?access_token=pk.eyJ1IjoianVhbm1hdHRlbyIsImEiOiJja3VwdTJ3OHI0bzA4MndvOGtvZWhrY20wIn0.c2s4TtrNnvq0YfiXGoXp-g&limit=1'
 
-
 request({ url : geocodeURL, json:true}, (error, response) => {
     if (error){
         console.log('Unable to connect to the location services!')
