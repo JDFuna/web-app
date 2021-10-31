407bd63f299ae3be5d866e1fc5c26cd5c72b7922
Judy Dolot Funa <89194168+JDFuna@users.noreply.github.com>
2021-10-31 17:44:43 +0700

    weather stack API

--- weather-app/app.js
+++ weather-app/app.js
@@ -1,12 +1,12 @@
-console.log('Starting')
+const request = require('request')
 
-setTimeout(() => {
-    console.log("2 second Timer")
-}, 2000)
+const url = 'http://api.weatherstack.com/current?access_key=a3371e89db999a6f68a7a908b9195a56&query=12.6732203,123.8749691'
 
-setTimeout(() =>{
-    console.log("0 Second Timer")
-},0)
+request({ url: url, json:true },(error, response) => {
+    // console.log(response.body.current.temperature)
+    const temperature = response.body.current.temperature
+    const feelslike = response.body.current.feelslike
+    const weather_descriptions = response.body.current.weather_descriptions[0]
+    console.log(weather_descriptions+ ". It is currently "+ temperature + " degress out. It feels like "+feelslike+" degrees out.")
 
-console.log('Stopping') 
-
+})
